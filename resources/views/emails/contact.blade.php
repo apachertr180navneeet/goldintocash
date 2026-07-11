9482cc541dccd3d968c560e87bfa5dbb5caf5966
apachertr180navneeet <navneet@brenin.co>
2026-07-11 17:45:36 +0530

    feat: add frontend website interactions and contact email template

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,18 @@
+@component('mail::message')
+# New Contact Message
+
+**Name:** {{ $data['name'] }}  
+**Email:** {{ $data['email'] }}  
+**Subject:** {{ $data['subject'] }}
+
+---
+
+**Message:**
+
+{{ $data['message'] }}
+
+---
+
+Thanks,  
+{{ config('app.name') }}
+@endcomponent
